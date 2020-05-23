0db95b7d34ec5d6f964464de1d65f187858e4ac8
Jose Gonzalez Riquelme <jozho.g@gmail.com>
2020-05-23 19:03:16 -0400

    Se comienza a trabajar en la lista, se deja trabajo hasta aqui por hoy

---
+++ app/src/main/java/com/example/appeva2josegonzalez/PersonFragment.kt
@@ -0,0 +1,105 @@
+package com.example.appeva2josegonzalez
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.appeva2josegonzalez.dummy.DummyContent
+import com.example.appeva2josegonzalez.dummy.DummyContent.DummyItem
+import com.example.appeva2josegonzalez.dummy.DummyContent.createDummyItem
+import kotlinx.android.synthetic.main.fragment_person.*
+
+/**
+ * A fragment representing a list of Items.
+ * Activities containing this fragment MUST implement the
+ * [PersonFragment.OnListFragmentInteractionListener] interface.
+ */
+open class PersonFragment : Fragment() {
+
+    // TODO: Customize parameters
+    private var columnCount = 1
+
+    private var listener: OnListFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            columnCount = it.getInt(ARG_COLUMN_COUNT)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_person_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = MyPersonRecyclerViewAdapter(DummyContent.ITEMS, listener)
+            }
+        }
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnListFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    fun OnListFragnentInteractonListener(listView: listView ) {
+        this.persona_datos.addChildrenForAccessibility(createDummyItem())
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson
+     * [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnListFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onListFragmentInteraction(item: DummyItem?)
+    }
+
+    companion object {
+
+        // TODO: Customize parameter argument names
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
+        @JvmStatic
+        fun newInstance(columnCount: Int) =
+            PersonFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
+                }
+            }
+    }
+}
