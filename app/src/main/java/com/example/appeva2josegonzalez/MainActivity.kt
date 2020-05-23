32d70d8a7570a707bc54638baa652645f3c5d70c
Jose Gonzalez Riquelme <jozho.g@gmail.com>
2020-05-23 17:04:11 -0400

    Se termina login, se termina navegacion entre las dos vistas, se empieza a pensar diseño de listview con todos los datos

--- app/src/main/java/com/example/appeva2josegonzalez/MainActivity.kt
+++ app/src/main/java/com/example/appeva2josegonzalez/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.appeva2josegonzalez
 
+import android.app.AlertDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.appeva2josegonzalez.R.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
+        val btn_ingresa = findViewById<Button>(id.Ingresa)
+        btn_ingresa.setOnClickListener{
+            var txtrut = findViewById<EditText>(id.txtrut)
+            var rut = txtrut.text.toString()
+            val alerta = AlertDialog.Builder(this)
+            if(rut ==""){
+                alerta.setMessage("Porfavor Ingrese Rut")
+                alerta.show()
+            }else{
+                Ingresa(rut, alerta)
+            }
+        }
     }
+
+    fun Ingresa(rut: String, alerta: AlertDialog.Builder){
+        var intent = Intent( this, listView::class.java )
+        if (valida(rut)){
+            startActivity(intent)
+        }else{
+            alerta.setMessage("Rut Incorrecto")
+            alerta.show()
+        }
+    }
+
 }
