42e77e416f5297451c57d452c31aaf14a222eb9e
Jose Gonzalez Riquelme <jozho.g@gmail.com>
2020-05-23 14:05:39 -0400

    Funcion valida rut pasada a Kotlin

--- app/src/main/java/com/example/appeva2josegonzalez/validaRut.kt
+++ app/src/main/java/com/example/appeva2josegonzalez/validaRut.kt
@@ -1,5 +1,46 @@
 package com.example.appeva2josegonzalez
 
-fun valida(rut: String){
+import java.lang.Math.round
 
+fun valida(rut: String): Boolean {
+    var rutvalida = ""
+    val constantesValidacion = arrayOf(3,2,7,6,5,4,3,2)
+    var suma = 0;
+
+    rutvalida = when(rut.length){
+        9 -> "0" + rut
+        8 -> "00" + rut
+        7 -> "000" + rut
+        6 -> "0000" + rut
+        else -> rut
+    }
+    println(rutvalida)
+
+    for (i in 0.. constantesValidacion.size -1 ){
+        suma += Character.getNumericValue(rutvalida[i]) * constantesValidacion[i]
+    }
+    var division : Float = suma.toFloat()/11
+    var divisionentero = division.toInt()
+    var resto = division - divisionentero
+    var digito = round(11- (11*resto))
+
+    var digitovalida = Character.toString(rutvalida[9])
+
+    if(digitovalida == "k"){
+        digitovalida = "9"
+    }
+    if(digitovalida == "0"){
+        digitovalida = "11"
+    }
+    return if (digito == digitovalida.toInt()) {
+        return true
+    } else {
+        if (digito.toInt() == 9) {
+            return false
+        } else if (digito.toInt() == 11) {
+            return false
+        } else {
+            false
+        }
+    }
 }
