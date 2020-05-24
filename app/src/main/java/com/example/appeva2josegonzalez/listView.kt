482a10216a11d247ce567981df53bfd38094fcbc
Jose Gonzalez Riquelme <jozho.g@gmail.com>
2020-05-24 00:44:05 -0400

    Se cambia de metodologia para mostrar resultados y el layout

--- app/src/main/java/com/example/appeva2josegonzalez/listView.kt
+++ app/src/main/java/com/example/appeva2josegonzalez/listView.kt
@@ -2,19 +2,15 @@ package com.example.appeva2josegonzalez
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.appeva2josegonzalez.dummy.DummyContent
 import kotlinx.android.synthetic.main.activity_list_view.*
+import java.text.FieldPosition
 
 class listView : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view)
-
-        fun onAttachFragment(fragment: PersonFragment){
-            if (fragment is PersonFragment) {
-                fragment.OnListFragnentInteractonListener(listView())
-            }
-        }
 
     }
 
